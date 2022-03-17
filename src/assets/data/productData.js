35e82370e5425e894893ad68c27285cb5be63aa2
Fry-Fr <jwalker.ry@gmail.com>
2022-03-17 06:09:46 -0400

    modified descriptions

--- src/assets/data/productData.js
+++ src/assets/data/productData.js
@@ -5,7 +5,7 @@ const products = [
     image_url: 'https://images.unsplash.com/photo-1512428813834-c702c7702b78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
     price: '10.99',
     item_name: 'Bonsai',
-    description: 'Made from ' + 'corn, '.repeat(7) + 'tortillas.'
+    description: 'Japanese "bonsai" only attempts to produce small trees that mimic the shape of real life trees.'
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const products = [
     image_url: 'https://images.unsplash.com/photo-1508748169069-82590c9f26e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
     price: '$3.99',
     item_name: 'Seeds',
-    description: 'Chewy and ' + 'fresh '.repeat(20)
+    description: 'Seeds are an embryonic plant enclosed in a protective outer covering.'
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const products = [
     image_url: 'https://images.unsplash.com/photo-1644826447754-ec3a4fa82b7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=985&q=80',
     price: '$7.99',
     item_name: 'Seedlings',
-    description: '... '.repeat(7)
+    description: 'A young sporophyte developing out of a plant embryo from a seed.'.repeat(2)
   },
   {
     id: 4,
