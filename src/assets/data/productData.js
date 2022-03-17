216dbd6d4eaa717bf4c8a1acea19f89ac5bfc8aa
Fry-Fr <jwalker.ry@gmail.com>
2022-03-17 03:34:44 -0400

    twelve objects in data list

--- src/assets/data/productData.js
+++ src/assets/data/productData.js
@@ -1,6 +1,7 @@
 const products = [
   {
     id: 1,
+    item_type: 'trees',
     image_url: 'https://images.unsplash.com/photo-1512428813834-c702c7702b78?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
     price: '10.99',
     item_name: 'Bonsai',
@@ -8,6 +9,7 @@ const products = [
   },
   {
     id: 2,
+    item_type: 'seeds',
     image_url: 'https://images.unsplash.com/photo-1508748169069-82590c9f26e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
     price: '$3.99',
     item_name: 'Seeds',
@@ -15,11 +17,84 @@ const products = [
   },
   {
     id: 3,
+    item_type: 'seedlings',
     image_url: 'https://images.unsplash.com/photo-1644826447754-ec3a4fa82b7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=985&q=80',
     price: '$7.99',
     item_name: 'Seedlings',
     description: '... '.repeat(7)
-  }
+  },
+  {
+    id: 4,
+    item_type: 'trees',
+    image_url: 'https://images.unsplash.com/photo-1631551807946-14b5fda1123b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    price: '$80.99',
+    item_name: 'Australia tree',
+    description: 'Northern Territory, Australia'
+  },
+  {
+    id: 5,
+    item_type: 'trees',
+    image_url: 'https://images.unsplash.com/photo-1610087557362-e070d3c5d07d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    price: '$30.00 per tree',
+    item_name: 'Maldives',
+    description: 'Maldives'
+  },
+  {
+    id: 6,
+    item_type: 'flowers',
+    image_url: 'https://images.unsplash.com/photo-1469259943454-aa100abba749?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    price: '$8.99',
+    item_name: 'Flowers',
+    description: 'Floral'
+  },
+  {
+    id: 7,
+    item_type: 'flowers',
+    image_url: 'https://images.unsplash.com/photo-1519378058457-4c29a0a2efac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1004&q=80',
+    price: '$8.99',
+    item_name: 'Roses',
+    description: 'Floral'
+  },
+  {
+    id: 8,
+    item_type: 'flowers',
+    image_url: 'https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    price: '$10.99',
+    item_name: 'Yellow Flowers',
+    description: 'Floral arrangement'
+  },
+  {
+    id: 9,
+    item_type: 'seedlings',
+    image_url: 'https://images.unsplash.com/photo-1505235687559-28b5f54645b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2232&q=80',
+    price: '$10.99',
+    item_name: 'Seedling',
+    description: 'seedling baby plant'
+  },
+  {
+    id: 10,
+    item_type: 'seedlings',
+    image_url: 'https://images.unsplash.com/photo-1466692476868-aef1dfb1e735?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    price: '$10.99',
+    item_name: 'Seedling',
+    description: 'seedling baby sprout a garden'
+  },
+  {
+    id: 11,
+    item_type: 'seeds',
+    image_url: 'https://images.unsplash.com/photo-1542990253-a781e04c0082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1694&q=80',
+    price: '$6.99',
+    item_name: 'Assortment Seeds/Nuts',
+    description: 'Seeds and nuts for consumption.'
+  },
+  {
+    id: 12,
+    item_type: 'seeds',
+    image_url: 'https://images.unsplash.com/photo-1604768802835-899055f0e245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    price: '$6.99',
+    item_name: 'Chai Seeds',
+    description: 'Seeds are good.'
+  },
 ];
 
 export default products;
