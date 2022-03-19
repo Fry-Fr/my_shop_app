c5bb9e94ce90c99275a9f2619679c044d1ffdb25
Fry-Fr <jwalker.ry@gmail.com>
2022-03-19 03:50:31 -0400

    styled Item page

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -1,11 +1,40 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import shopData from '../assets/data/productData';
+import { Card } from 'antd';
 import styled from 'styled-components';
+import shopData from '../assets/data/productData';
 
 const MainContainer = styled.div`
 img {
+  border-radius: 1rem;
+  width: 100%;
+  max-height: 140mm;
+}
+.ant-card {
+  background-color: rgb(var(--primary-color));
+  border-radius: .75rem;
   width: 50%;
+  margin: 0 auto 1rem;
+  .ant-card-body {
+    padding: 1rem;
+  }
+  .item-description {
+    text-align: left;
+    border-top: 1px solid black;
+    padding: 1rem 1rem 0;
+  }
+  .item-details {
+    display: flex;
+    justify-content: space-evenly;
+    padding: .5rem 0;
+    .item-price {
+      font-weight: bold;
+    }
+    .item-name {
+      font-weight: bold;
+      text-decoration: underline;
+    }
+  }
 }
 `;
 
@@ -27,10 +56,15 @@ function Item({ dispatch, setCart }) {
     <MainContainer>
       {item.map((_) => {
         return (
-          <div key={_.id}>
+          <Card key={_.id}>
             <img src={_.image_url} alt='' />
+            <div className='item-details'>
+              <span className='item-name'>{_.item_name}</span>
+              <span className='item-price'>price: {_.price}</span>
+            </div>
+            <p className='item-description'>{_.description}</p>
             <button onClick={handleAddToCart}>add to cart</button>
-          </div>
+          </Card>
         );
       })}
     </MainContainer>
